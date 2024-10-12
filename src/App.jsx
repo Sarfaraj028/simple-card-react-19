fc2b0cf987c733e79bbc459a9dde9754fa0a0f45
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-10-12 10:51:55 +0530

    finally project made

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <div className="card-container">
+          <img src="moon-night.webp" width={"500px"} alt="cover-image" />
+          <h2>Moon Knight </h2>
+          <p>
+            <strong>Genres: </strong> Horror, Adventure, Thriller, Action,
+            Science FIction, Darama.
+          </p>
+          <p>
+            <strong>Summary: </strong>
+            Steven Grant, a gift shop employee who discovers he has dissociative
+            identity disorder and shares a body with mercenary Marc Spector.
+          </p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
