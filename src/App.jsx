b8421f425ec7c01f69d4e39ab253d69904ab23cb
Sarfaraj028 <sarfaraja0869@gmail.com>
2024-10-12 11:23:33 +0530

    card updated just used the feature of component reusibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
+import FavSeries from "./components/FavSeries";
 function App() {
   return (
     <>
       <div className="container">
-        <div className="card-container">
-          <img src="moon-night.webp" width={"500px"} alt="cover-image" />
-          <h2>Moon Knight </h2>
-          <p>
-            <strong>Genres: </strong> Horror, Adventure, Thriller, Action,
-            Science FIction, Darama.
-          </p>
-          <p>
-            <strong>Summary: </strong>
-            Steven Grant, a gift shop employee who discovers he has dissociative
-            identity disorder and shares a body with mercenary Marc Spector.
-          </p>
-        </div>
+        <FavSeries />
+        <FavSeries />
       </div>
     </>
   );
---
+++ src/components/FavSeries.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function FavSeries() {
+  return (
+    <>
+      <div className="card-container">
+        <img src="moon-night.webp" width={"100%"} alt="cover-image" />
+        <h2>Moon Knight </h2>
+        <p>
+          <strong>Genres: </strong> Horror, Adventure, Thriller, Action, Science
+          FIction, Darama.
+        </p>
+        <p>
+          <strong>Summary: </strong>
+          Steven Grant, a gift shop employee who discovers he has dissociative
+          identity disorder and shares a body with mercenary Marc Spector.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default FavSeries;
